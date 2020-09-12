eb9a73e57a40ecee2cfbf436f507ea249f4c8f12
niktoto315 <62284029+niktoto315@users.noreply.github.com>
2020-09-12 20:31:57 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 Elements = {
-'pars': [document.querySelector('par1')],
-'buttons': [document.querySelector('but1')],
+'pars': [document.querySelector('#par1')],
+'buttons': [document.querySelector('#but1')],
 }
 
-State = {
+Game = {
 'Count': 0,
 'passPlus': 1,
 'butPlus': 10,
 }
 
 function Click1(){
-State['Count'] += State['butPlus'];
+Game['Count'] += Game['butPlus'];
 }
 
 function MainTimer(){
-State['Count'] += State['passPlus'];
-Elements['pars'][0] = State['Count'];
+Game['Count'] += Game['passPlus'];
+Elements['pars'][0].innerHTML = Game['Count'];
 }
 
 setInterval(MainTimer, 1000);
